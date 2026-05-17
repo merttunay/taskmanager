6010695fcacf1d7abc6cf53e8f242e6b2937bb74
merttunay <merttunay03@icloud.com>
2026-05-17 23:38:56 +0300

    Task entity ve repository eklendi

---
+++ src/main/java/org/example/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package org.example.taskmanager.repository;
+
+import org.example.taskmanager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
